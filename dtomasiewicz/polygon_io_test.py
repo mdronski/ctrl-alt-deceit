b367eaf6ebac9c431c0ded715ed3ff5fb20083b8
Dawid Tomasiewicz <tomasiewicz.dawid@gmail.com>
2025-08-18 08:54:55 +0200

    add polygon io

---
+++ dtomasiewicz/polygon_io_test.py
@@ -0,0 +1,16 @@
+from polygon import RESTClient
+
+client = RESTClient(api_key="doYsnLZqJmyklTl8ifPYqCGhJqaYvhyR")
+
+news_articles = client.list_ticker_news(
+	"AAPL",
+	params={"published_utc.gte": "2025-07-03"},
+	order="desc",
+	limit=2
+	)
+
+for article in news_articles:
+    print(f"{article.title} [Insights: {article.insights}]")
+
+
+
